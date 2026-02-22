f67e45d6e2e68614316d89d2ccf15cd8ce826ba4
Vikas Rana <vikasranarsr.cse@sce.org.in>
2026-02-22 13:24:01 +0530

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 
-import Landing from './page/landing'
+import Landing from './page/Landing'
 import Layout from './layout/layout'
 
 
